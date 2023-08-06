ce0e90e6426ef9e8140d87687e1c8b829ca9a4cf
Jose Moreno <jose.moreno@microsoft.com>
2023-08-06 09:00:37 +0200

    consolidated to backend

--- azpolicy/backend.tf
+++ azpolicy/backend.tf
@@ -1,12 +1,17 @@
 terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">=3.49.0"
-    }
+  backend "azurerm" {
+    resource_group_name  = "erjositotfstate"
+    storage_account_name = "erjositotfstate"
+    container_name       = "azpolicy"
+    key                  = "azpolicy.tfstate"
   }
 }
-
+ 
 provider "azurerm" {
+  # The "feature" block is required for AzureRM provider 2.x.
+  # If you're using version 1.x, the "features" block is not allowed.
+  version = "~>2.0"
   features {}
 }
+ 
+data "azurerm_client_config" "current" {}
