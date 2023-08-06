6bd0a155a0a80a2de1dc3ad0c7f9cbc27f8a7208
Jose Moreno <jose.moreno@microsoft.com>
2023-08-06 18:36:09 +0200

    added assignment

--- basic-policy/main.tf
+++ basic-policy/main.tf
@@ -36,7 +36,7 @@ resource "azurerm_policy_set_definition" "resource_location" {
   policy_type           = "Custom"
   display_name          = "Resource Location"
   management_group_id   = var.definition_management_group
-  parameters = file("${path.module}/initiative-parameters.json")
+  parameters            = file("${path.module}/initiative-parameters.json")
   policy_definition_reference {
     policy_definition_id = azurerm_policy_definition.resource_location.id
     parameter_values = jsonencode({
@@ -45,4 +45,14 @@ resource "azurerm_policy_set_definition" "resource_location" {
       }
     })
   }
+}
+
+# Assignment
+resource "azurerm_subscription_policy_assignment" "resource_location" {
+  name                 = "Audit resources in West Europe"
+  scope                = var.definition_management_group
+  policy_definition_id = azurerm_policy_set_definition.resource_location.id
+  description          = "Audit resources in West Europe"
+  display_name         = "Audit resources in West Europe"
+  parameters           = file("${path.module}/assignment-parameters.json")
 }
