d44e1fab6c794d646afc4d9e7f5484a754a1f784
Jose Moreno <jose.moreno@microsoft.com>
2023-08-06 08:56:38 +0200

    changes

--- azpolicy/definitions.tf
+++ azpolicy/definitions.tf
@@ -1,3 +1,21 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "erjositotfstate"
+    storage_account_name = "erjositotfstate"
+    container_name       = "azpolicy"
+    key                  = "azpolicy.tfstate"
+  }
+}
+ 
+provider "azurerm" {
+  # The "feature" block is required for AzureRM provider 2.x.
+  # If you're using version 1.x, the "features" block is not allowed.
+  version = "~>2.0"
+  features {}
+}
+ 
+data "azurerm_client_config" "current" {}
+
 module "dine_dns_zone_group" {
   source              = "..//modules/definition"
   policy_name         = "dnszonegroup_dine"
