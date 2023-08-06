af38eab7fc633403f1d98bce1f948885238fe8ba
Jose Moreno <jose.moreno@microsoft.com>
2023-08-06 09:17:49 +0200

    removed tf provider version constraint

--- azpolicy/backend.tf
+++ azpolicy/backend.tf
@@ -8,9 +8,6 @@ terraform {
 }
  
 provider "azurerm" {
-  # The "feature" block is required for AzureRM provider 2.x.
-  # If you're using version 1.x, the "features" block is not allowed.
-  version = "~>2.0"
   features {}
 }
  
