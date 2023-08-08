dd8a5a36006edc45739c48d96700f90a2ae74c45
Jose Moreno <jose.moreno@microsoft.com>
2023-08-08 15:16:29 +0200

    for loop in initiative definition

--- dnszonegroup-policy/main.tf
+++ dnszonegroup-policy/main.tf
@@ -37,7 +37,7 @@ resource "azurerm_policy_set_definition" "zone_group" {
   policy_type           = "Custom"
   display_name          = "Zone Group for endpoints"
   management_group_id   = var.definition_management_group
-  parameters            = file("${path.module}/initiative-parameters.json")
+  parameters            = jsondecode({for s in var.endpoint_types : "${s.value}PrivateDnsZoneId" => jsonencode(replace(var.initiative_param_template, "_ENDPOINT_TYPE_", s.value))})
   dynamic policy_definition_reference {
     for_each = toset(var.endpoint_types)
     content {
--- dnszonegroup-policy/variables.tf
+++ dnszonegroup-policy/variables.tf
@@ -7,3 +7,16 @@ variable "endpoint_types" {
   description = "List of endpoint types to create policies for"
   default     = ["blob", "table", "queue", "file"]
 }
+variable "initiative_param_template" {
+  type = string
+  description = "JSON Template for creating initiative parameters"
+  default = <<PARAM_TEMPLATE
+    "type": "String",
+    "metadata": {
+        "displayName": "Private DNS Zone ID",
+        "description": "Private DNS Zone ID",
+        "strongType": "Microsoft.Network/privateDnsZones"
+    }
+  }
+PARAM_TEMPLATE
+}
