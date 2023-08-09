82fd32c625523d3b621fd508dc03c57965371214
Jose Moreno <jose.moreno@microsoft.com>
2023-08-09 20:49:28 +0200

    removed terraform.tfvars

--- modules/dns-zone-group/terraform.tfvars
+++
@@ -1,4 +0,0 @@
-# Sample values
-definition_management_group = "/providers/Microsoft.Management/managementGroups/mymgmtgroup"
-zone_subscription_id = "e7da9914-9b05-4891-893c-546cb7b0422e"
-zone_rg_name = "dns"
--- modules/dns-zone-group/variables.tf
+++ modules/dns-zone-group/variables.tf
@@ -2,11 +2,6 @@ variable "definition_management_group" {
   type        = string
   description = "Management group where policies and initiatives are defined"
 }
-variable "endpoint_types" {
-  type        = list(string)
-  description = "List of endpoint types to create policies for"
-  default     = ["blob", "table", "queue", "file"]
-}
 variable "zone_rg_name" {
   type        = string
   description = "Name of the private DNS zone resource group"
